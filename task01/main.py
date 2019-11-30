e681b704758d8ad3450d2900674bbe0556b8bb45
Roman Khomenko <romankhomenko1995@gmail.com>
2019-11-30 12:18:43 +0300

    Рефакторинг

--- task01/main.py
+++ task01/main.py
@@ -1,36 +1,43 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
-
-if not cap.isOpened():
-    raise IOError("Cannot open web camera")
-
-ESC = 27
-
 
 def draw_circle(x, frame):
     cv2.circle(frame, center=(int(x[1]), int(x[0])), radius=5, color=[0, 0, 255], thickness=-1)
 
 
-while True:
-    ret, frame = cap.read()
+def main():
+    cap = cv2.VideoCapture(0)
 
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if not cap.isOpened():
+        raise IOError("Cannot open web camera")
 
-    frame_gray = np.float32(frame_gray)
-    dst = cv2.cornerHarris(frame_gray, 2, 3, 0.04)
+    ESC = 27
 
-    idx1, idx2 = np.where(dst > 0.01 * dst.max())
-    index = np.column_stack((idx1, idx2))
+    while True:
+        ret, frame = cap.read()
 
-    np.apply_along_axis(lambda row: draw_circle(row, frame), axis=1, arr=index)
+        # Использование детектора Харриса на чб кадре
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_gray = np.float32(frame_gray)
+        dst = cv2.cornerHarris(frame_gray, 2, 3, 0.04)
 
-    cv2.imshow('Input', frame)
+        # Получение координат углов
+        idx1, idx2 = np.where(dst > 0.01 * dst.max())
+        index = np.column_stack((idx1, idx2))
 
-    c = cv2.waitKey(1)
-    if c == ESC:
-        break
+        # Отрисовка точек на кадре по координатам углов
+        np.apply_along_axis(lambda row: draw_circle(row, frame), axis=1, arr=index)
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow('Input', frame)
+
+        c = cv2.waitKey(1)
+        if c == ESC:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
